c619fc3b2ffc5303f1768b10f4d12648e8a72d36
AhmedSBelal <ahmadsd77777@gmail.com>
2025-02-04 17:09:34 +0200

    updated migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('fcm_token')->nullable();
             $table->enum('gender', ['male', 'female']);
             $table->enum('roles', ['admin', 'customer', 'driver']);
             $table->string('mobile_number');
