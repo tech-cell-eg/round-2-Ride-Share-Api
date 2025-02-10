898fc9356d924b174a136bf2a8e1832038c79d0f
AhmedSBelal <ahmadsd77777@gmail.com>
2025-02-10 19:17:28 +0200

    make admin and offers migrations

---
+++ database/migrations/2025_02_10_171216_create_offers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('admin_id')->nullable()->constrained('admins')->references('admin_id')->onDelete('set null');
+            $table->string('title');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('offers');
+    }
+};
